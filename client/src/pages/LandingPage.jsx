7b1de433af95ea86e20799000c8ed27433365f27
KyleLorenzCasianan <ckylelorenz@gmail.com>
2025-10-25 13:30:49 +0800

    Web Update UI to Dark Theme

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import LandPageNav from "../Components/LandPageNav";
 import LandPageCenter from "../Components/LandPageCenter";
-
-
-
+import LandPageFeature from "../Components/LandPageFeature";
+import LandPageBottom from "../Components/LandPageBottom";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-base-200">
       <LandPageNav />
-      <div className="flex m-10">
-        <LandPageCenter />
-      </div>
+      <LandPageCenter />
+      <LandPageFeature />
+      <LandPageBottom />
     </div>
   );
 };
